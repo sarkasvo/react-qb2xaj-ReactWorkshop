0d82a6090002b56d72b18af93baa42bab25579d4
sarkasvo <sarka.svobodna@gmail.com>
2021-06-17 09:11:59 +0200

    formAddPets added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './style.css';
-
 import HeaderComponent from './header';
 import TableComponent from './table';
 import ButtonComponent from './button';
+import FormAddPet from './formAddPet.js';
 
 export default function App() {
   const initialPetList = [
@@ -31,11 +31,30 @@ export default function App() {
 
   const [pets, setPets] = React.useState(initialPetList);
 
+  // useState hook -- do inputValueName se ted zapise '', a updatuje se pri pouziti funkce setInputValueName
+  const [inputValueName, setInputValueName] = React.useState('');
+  const [inputValueType, setInputValueType] = React.useState('');
+
   // deletes the line whed the action is treiggered (in button file the action is defined as onClick)
   function handleRemove(index) {
     const newPetList = pets.filter((pet, i) => i != index);
     setPets(newPetList);
   }
+
+  // for the imput form for the new pet
+  // This is another way to create functions.  You can write them as function like above or like below
+  const handleAdd = () => {
+    const newPet = {
+      name: inputValueName,
+      type: inputValueType
+    };
+
+    const newPets = [...pets, newPet];
+
+    setPets(newPets);
+    setInputValueName('');
+    setInputValueType('');
+  };
 
   return (
     <div>
@@ -46,6 +65,13 @@ export default function App() {
         tableHeading2={heading2}
         tableTitle={title}
         onRemove={handleRemove}
+      />
+      <FormAddPet
+        inputValueName={inputValueName}
+        inputValueType={inputValueType}
+        setInputValueName={setInputValueName}
+        setInputValueType={setInputValueType}
+        handleAdd={handleAdd}
       />
       <ButtonComponent />
     </div>
